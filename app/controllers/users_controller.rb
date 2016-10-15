0bf0af2a45239e45395c66f61472995785c3ce41
ryosuke tsuihiji <tuiryo13@gmail.com>
2016-10-15 11:57:04 +0000

    Finish user edit, update, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -19,8 +21,40 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @User = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "編集しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end  
+      # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "削除しました"
+    redirect_to signup_url
+  end	
 end
